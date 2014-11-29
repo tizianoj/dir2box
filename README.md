8bcaf203207a0cf1802a3195008d1bbd1c105461
tizianoj <tizianoj@users.noreply.github.com>
2014-11-29 01:03:07 +0100

    First working example, without dropbox loading

--- README.md
+++ README.md
@@ -1,7 +1,12 @@
 dir2box
 =======
 
-A simple python daemon for uploading a filesystem directory to dropbox. Developed in order to upload files from my home surveillance ftp server (a RaspberryPi :-) ) as soon as files are uploaded.
+A simple python daemon for uploading a filesystem directory to dropbox. Developed in order to upload files from my home 
+surveillance ftp server (a RaspberryPi :-) ) as soon as files are uploaded.
+
+Python client based on: http://stackoverflow.com/questions/23894221/upload-file-to-my-dropbox-from-python-script
 
 Requirements: 
- * whatchdog
+ * pip install whatchdog
+ * pip install dropbox
+
--- dir2box/__init__.py
+++ dir2box/__init__.py
@@ -5,21 +5,27 @@ import sys
 import time
 import datetime
 import logging
-from watchdog.observers import Observer
-from watchdog.events import LoggingEventHandler
+import watchdog.observers
+import watchdog.events
+
+DIR_TO_MONITOR = "D:\\tmp"
+
+
+class MyFileHandler(watchdog.events.FileSystemEventHandler):
+    def on_created(self, event):
+        if not event.is_directory:
+            print("Created filename %s" % event.src_path)
 
 if __name__ == "__main__":
-    time_now=datetime.datetime.now()
-    full_date_str=time_now.strftime("%Y-%m-%d")
-    date_str=time_now.strftime("%Y-%m-%d_%H:%M:%S")
+    time_now = datetime.datetime.now()
+    full_date_str = time_now.strftime("%Y-%m-%d")
+    date_str = time_now.strftime("%Y-%m-%d_%H:%M:%S")
 
-    logging.basicConfig(level=logging.INFO,
-                        format='%(asctime)s - %(message)s',
-                        datefmt='%Y-%m-%d %H:%M:%S')
-    path = sys.argv[1] if len(sys.argv) > 1 else '.'
-    event_handler = LoggingEventHandler()
-    observer = Observer()
-    observer.schedule(event_handler, path, recursive=True)
+    # path = sys.argv[1] if len(sys.argv) > 1 else '.'
+    path = DIR_TO_MONITOR
+    event_handler = MyFileHandler()
+    observer = watchdog.observers.Observer()
+    observer.schedule(event_handler, path, recursive=False)
     observer.start()
     try:
         while True:
