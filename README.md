e1cad08d73e9efc89811a24642302d8ada66e350
tizianoj <tizianoj@users.noreply.github.com>
2014-11-30 15:13:01 +0100

    Removed config.py in favor of header configuration, for ease of install

--- README.md
+++ README.md
@@ -4,10 +4,10 @@ dir2box
 A simple python daemon for uploading a filesystem directory to dropbox. Developed in order to upload files from my home 
 surveillance ftp server (a RaspberryPi :-) ) as soon as files are uploaded.
 
-Create your own application at https://www.dropbox.com/developers/app, get an access token and save it into config.py.
-App has to be able to work with Core APIs and to manage files.
+Create your own application at https://www.dropbox.com/developers/app, get an access token and save it into the header
+of file at the ACCESS_TOKEN constant. App has to be able to work with Core APIs and to manage files.
 
-Then all the files in DIR_TO_MONITOR (settings in config.py) will be watched. When a new file arrives there, a
+Check configuration in file header: all the files in DIR_TO_MONITOR will be watched. When a new file arrives there, a
 subdirectory with date pattern will be created and file will be time prefixed and moved there. An identical structure
 will be replicated and uploaded to dropbox under BASE_DIR_TO_UPLOAD with same structure.
 
--- dir2box.py
+++ dir2box.py
@@ -5,11 +5,24 @@ import os.path
 import time
 import datetime
 import dropbox
+import logging
 import shutil
 import watchdog.observers
 import watchdog.events
 
-from config import *
+######################## CONFIGURATION START ########################
+
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s:%(levelname)s - %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
+
+# Obtain one for your personal app from https://www.dropbox.com/developers/apps
+# App has to be able to work with Core APIs and to manage files.
+ACCESS_TOKEN="<insert your access token here>"
+
+DIR_TO_MONITOR = "/media/disk/mylocaldir"
+BASE_DIR_TO_UPLOAD = "/my_cloud_dir"
+WAIT_UPLOAD_MAX_TIME_SECONDS = 60
+
+######################### CONFIGURATION END #########################
 
 try:
     from config_local import *
@@ -92,7 +105,7 @@ class FileHandler2Box(watchdog.events.FileSystemEventHandler):
 
 if __name__ == "__main__":
     if ACCESS_TOKEN == "<insert your access token here>":
-        print("Please configure the program editing config.py file!")
+        print("Please configure the program editing the configuration lines!")
         exit(-1)
     event_handler = FileHandler2Box()
     observer = watchdog.observers.Observer()
